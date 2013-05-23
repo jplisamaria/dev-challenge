d415b68075d31041562d6a938528ebe6dcefddd1
jplisamaria <jplisamaria@gmail.com>
2013-05-23 10:25:45 -0400

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,164 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name: Lisa-Maria Mehta
+
+
+# ******************
+# ** Exercise 2.1 **
+# ******************
+# A leading '0' indicates that the following int should be read
+# in base 8.  A syntax error is generated if any digit 
+# following the leading zero is greater than 7.
+
+
+
+
+
+# ******************
+# ** Exercise 2.2 **
+# ******************
+# Using Python Interpreter
+# -----------------------
+# >>>  5
+# 5
+# >>>  x=5
+# >>>  x+1
+# 6
+#
+# Using the script
+# ----------------
+print
+print ('Exercise 2.2')
+# The following script
+print (5)
+x = 5
+print (x+1)
+# generates the following output upon running:
+# 5
+# 6
+
+
+
+
+
+# ******************
+# ** Exercise 2.3 **
+# ******************
+# 	Some Easy Math
+#	--------------
+#		>>> width = 17
+#		>>> height = 12
+#		>>> delimiter = '.'
+#	1.	>>> width/2
+#		8
+#		>>>
+#	2.	>>> width/2.0
+#		8.5
+#		>>>
+#	3.	>>> height/3
+#		4
+#		>>>
+#	4.	>>> 1+2*5
+#		11
+#		>>>
+#	5.	>>> delimiter *5
+#		'.....'
+
+
+
+
+
+# ******************
+# ** Exercise 2.4 **
+# ******************
+#	1.	Volume of Sphere radius = 5 (using interpretter)
+#		------------------------------------------------
+#			>>> pi = 3.1415926535897932
+#			>>> r = 5
+#			>>> vol = (4/3)*pi*r**2
+#			>>> vol
+#			78.53981633974483
+#		Volume = 78.54
+#
+#	2.	Wholesale cost of Book Order (using interpretter)
+#		-------------------------------------------------
+#			>>> single_price = 24.95
+#			>>> number_ordered = 60
+#			>>> bookcost = single_price * number_ordered
+#			>>> bookcost
+#			1497.0
+#			>>> discount = 0.4
+#			>>> 
+#			>>> ship1st = 3
+#			>>> shiprest = 0.75
+#			>>> shipping = ship1st + (number_ordered -1)*shiprest
+#			>>> shipping
+#   	    47.25
+#			>>> wholesale = bookcost*discount + shipping
+#			>>> wholesale
+#			731.5500000000001
+#		Total cost of order is $731.55
+#
+#	3a.	What time do I get home? (using interpretter)
+#		---------------------------------------------
+#			>>> easypace = 8.0 + 15/60.0   		# in min/mile
+#			>>> tempopace = 7.0 + 12/60.0		# in min/mile
+#			>>> runtime_mins = (1*easypace) + (3*tempopace) + (1*easypace) 
+#			>>> runtime_mins      # in minutes
+#			38.1
+#			>>>
+#			>>> starttime = 6 + 52/60.0   #Starttime in hours
+#			>>> endtime = startime + runtime_hours
+#			>>> endtime_hour
+#			7.501666666666667
+#		
+#			>>> end_hour = endtime_hour - 0.501666666666667
+#			>>> end_hour
+#			7.0
+#			>>> 
+#			>>> endtime_mins = (endtime_hour - end_hour) *60
+#			>>> endtime_mins 
+#			30.100000000000016
+#			>>> end_mins = endtime_mins -0.100000000000016
+#			>>> end_mins
+#			30.0
+#			>>>
+#			>>> endtime_secs = (endtime_mins- end_mins) *60
+#			>>> endtime_secs
+#			6.000000000000938
+#			>>> end_secs = endtime_secs - 0.000000000000938
+#			>>> end_secs
+#			6.0
+#		I return home at 7:30:06 am.
+#		
+#	3b.	What time do I get home? using (using script)
+#		---------------------------------------------
+print
+print ('Exercise 2.4.3')
+start_hour = 6
+start_mins = 52
+# Convert starttime to hours with mins as a decimal
+starttime = start_hour + start_mins/60.0 
+print ('startime ='), start_hour, (':'), start_mins , ('a.m.')
+
+# Equations determning duration of run
+easypace = 8.0 + 15/60.0 								# in min/mile
+tempopace = 7.0 + 12/60.0								# in min/mile
+runtime = (1*easypace) + (3*tempopace) + (1*easypace) 	# in mins
+runtime_in_hours = runtime/60	
+runtime_mins = int(runtime)
+runtime_secs = int((runtime - runtime_mins) * 60) 
+
+print ('runtime ='), runtime_mins, ('minutes and'),
+print runtime_secs, ('seconds.')
+
+endtime = starttime + runtime_in_hours				#in hours
+endtime_hour = int(endtime)
+endtime_mins = int((endtime-endtime_hour)*60)
+endtime_secs = int((((endtime - endtime_hour)*60) - endtime_mins) *60)
+
+print
+print ('If I start running at'), start_hour, (':'), start_mins,
+print ('a.m., I will')
+print ('be done by'), endtime_hour ,(':'), endtime_mins,
+print ('a.m. and'), endtime_secs, ('seconds.')
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,159 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name: Lisa-Maria Mehta
+
+
+# ******************
+# ** Exercise 3.1 **
+# ******************
+# Calling a function before it's been defined
+# -------------------------------------------
+#
+#repeat_lyrics()
+#
+#def print_lyrics():
+#	print "I'm a lumberjack, and I'm OK."
+#	print "I sleep all night and I work all day."
+#
+#def repeat_lyrics():
+#	print_lyrics()
+#	print_lyrics()
+#
+#
+# Placing the repeat_lyrics() function call at the beginning
+# yields the following error:
+#		Traceback (most recent call last):
+#		  File "chapter3_exercises.py", line 9, in <module>
+#		    repeat_lyrics()
+#		NameError: name 'repeat_lyrics' is not defined
+
+
+
+
+
+# ******************
+# ** Exercise 3.2 **
+# ******************
+# Changing order of function definitions
+# --------------------------------------
+print
+print ('Exercise 3.2')
+
+def repeat_lyrics():
+	print_lyrics()
+	print_lyrics()
+
+def print_lyrics():
+	print "I'm a lumberjack, and I'm OK."
+	print "I sleep all night and I work all day."
+
+repeat_lyrics()
+# Changing the order of the function definitions has no apparent
+# negative effect on the execution of the program.
+
+
+
+
+
+# ******************
+# ** Exercise 3.3 **
+# ******************
+# Right Justify
+# -------------
+print
+print ('Exercise 3.3')
+word = ('allen')
+x = len(word)
+print ('.')*70   #as a visual aid to determine line length
+space = (' ')*(70-x)
+cat = space + word
+print cat
+print
+
+
+
+
+
+# ******************
+# ** Exercise 3.4 **
+# ******************
+# Using function objects
+# ----------------------
+#	1.	Typing and testing script
+#		-------------------------
+print ('Exercise 3.4.1')
+
+def do_twice(f):
+	f()
+	f()
+
+def print_spam():
+	print 'spam'
+
+do_twice(print_spam)	
+
+
+#	2.	Passing two arguments
+#       ---------------------
+print
+print ('Exercise 3.4.2')
+
+def do_twice2(f2, word):
+	f2(word)
+	f2(word)
+
+def print_spam2(word):
+	print word
+
+do_twice2(print_spam2, 'spam2')
+
+#	3.	General print_twice function
+#       ----------------------------
+print
+print ('Exercise 3.4.3')
+
+def do_twice3(f3):
+	f3()
+	f3()
+
+def print_twice3():
+	print 'spam3'
+	print 'spam3'
+
+do_twice3(print_twice3)
+
+
+#	4.	Passing two arguments with print_twice function
+#       -----------------------------------------------
+print
+print ('Exercise 3.4.4')
+
+def do_twice4(f4, word4):
+	f4(word4)
+	f4(word4)
+
+def print_twice4(word4):
+	print word4
+	print word4
+
+do_twice4(print_twice4, 'spam4')
+
+
+#	.	do_four function
+#       ----------------
+print
+print ('Exercise 3.4.5')
+
+def do_twice5(f5, word5):
+	f5(word5)
+	f5(word5)
+
+def do_four(do_twice5, word5):
+	do_twice5 (word5)
+	do_twice5 (word5)
+
+def print_twice5(word5):
+	print word5
+	print word5
+
+do_four(print_twice5, 'spam5')
